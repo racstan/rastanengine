cc102c999c56bbf5c103476023a42e6ef7273a98
Racstan <asthanarachit@gmail.com>
2024-09-28 09:24:49 +0530

    Updated resources.py

--- engine/resources.py
+++ engine/resources.py
@@ -1,3 +1,4 @@
+# engine/resources.py
 import pygame
 import os
 
@@ -5,10 +6,24 @@ class ResourceManager:
     def __init__(self):
         self.images = {}
         self.sounds = {}
+        self.base_path = os.path.join(os.getcwd(), 'assets')
 
     def load_resources(self):
-        # Load images
-        self.images['zeus'] = pygame.image.load(os.path.join('assets', 'images', 'zeus.png')).convert_alpha()
-        self.images['titan'] = pygame.image.load(os.path.join('assets', 'images', 'titan.png')).convert_alpha()
-        # Load sounds
-        # self.sounds['attack'] = pygame.mixer.Sound(os.path.join('assets', 'sounds', 'attack.wav'))
+        self.load_images()
+        self.load_sounds()
+
+    def load_images(self):
+        images_path = os.path.join(self.base_path, 'images')
+        for filename in os.listdir(images_path):
+            if filename.endswith('.png'):
+                path = os.path.join(images_path, filename)
+                image = pygame.image.load(path).convert_alpha()
+                self.images[filename[:-4]] = image  # Remove .png extension
+
+    def load_sounds(self):
+        sounds_path = os.path.join(self.base_path, 'sounds')
+        for filename in os.listdir(sounds_path):
+            if filename.endswith('.wav'):
+                path = os.path.join(sounds_path, filename)
+                sound = pygame.mixer.Sound(path)
+                self.sounds[filename[:-4]] = sound
