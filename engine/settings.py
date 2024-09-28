647edaac19c1197f9e2d7d369bd5cd96dc4a7401
Racstan <asthanarachit@gmail.com>
2024-09-28 09:15:05 +0530

    Addition of settings.py

---
+++ engine/settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    def __init__(self):
+        self.title = "Rastan Engine"
+        self.screen_width = 800
+        self.screen_height = 600
+        self.fps = 60
+        self.background_color = (0, 0, 0)  # Black
