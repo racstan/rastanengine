56463f6da1aa56949124246869a866f93a7b90c4
Racstan <asthanarachit@gmail.com>
2024-09-28 09:21:38 +0530

    Update input.py

--- engine/input.py
+++ engine/input.py
@@ -1,12 +1,18 @@
 import pygame
 
-class InputHandler:
+class InputSystem:
     def __init__(self):
         self.quit = False
         self.keys_pressed = {}
+        self.mouse_buttons = {}
+        self.mouse_position = (0, 0)
 
     def handle_events(self):
         self.keys_pressed = pygame.key.get_pressed()
+        self.mouse_buttons = pygame.mouse.get_pressed()
+        self.mouse_position = pygame.mouse.get_pos()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.quit = True
+            # Handle other events like keydown, mouse clicks, etc.
